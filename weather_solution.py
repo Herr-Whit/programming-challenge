1233932d6d2d3f09d3b906a604515cc66f3464f8
Tony <anton.whittaker@gmail.com>
2021-05-01 17:18:28 +0200

    converted the notebook solution in a python function
    removed some comment and formatted the file

---
+++ weather_solution.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+Weather Data Challenge
+In weather.csv you’ll find the daily weather data of a single month. Read the file, then output the day number
+(column one Day) of the day with the smallest temperature spread (difference between maximum & minimum temperature of
+the day.) The maximum temperature is the second column MxT, the minimum temperature the third column MnT.
+"""
+import pandas as pd
+
+
+def find_highest_tdiff():
+    """
+    Solution to the weather data challenge. Finds the highest temparature difference in weather data and identifies the
+    day
+    :return: day - integer specifying the day in the month with the highest temparature difference.
+    """
+    # Transform the file in the "Pandas" DataFrame format
+
+    source_file = r'src\main\resources\de\exxcellent\challenge\weather.csv'
+    df = pd.read_csv(source_file)
+
+    # Calculate the difference in Temparature for each day.
+
+    df['TDiff'] = df['MxT'] - df['MnT']
+
+    # Check for each day, whether it is one of the days with the highest temparature difference.
+
+    maximum_temparature = max(df['TDiff'])
+
+    ismax = df['TDiff'] == maximum_temparature
+
+    # Select datapoints, which reflect the highest temparature differences using boolean indexing.
+    # Isolate the "Day" entry.
+
+    df_with_highest_diff = df[ismax]
+
+    day = df_with_highest_diff["Day"].iloc[0]
+    return day
+
