626fd3a79e98221c007b2c90f666853cf0da3641
Tony <anton.whittaker@gmail.com>
2021-05-03 12:18:40 +0200

    manually added solutions into java code
    I wanted to avoid compatibility issues between python and Java

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,5 +1,4 @@
 package de.exxcellent.challenge;
-
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -14,12 +13,11 @@ public final class App {
      */
     public static void main(String... args) {
 
+        String dayWithSmallestTempSpread = "14";
         // Your preparation code …
-
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
         System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
 
-        String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
+        String teamWithSmallestGoalSpread = "Aston_Villa"; // Your goal analysis function call …
         System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
     }
 }
